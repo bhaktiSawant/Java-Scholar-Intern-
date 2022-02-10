f9245cbcb9908c40c8d4d31aad4e32520165229b
Bhakti Sawant <71435451+bhaktiSawant@users.noreply.github.com>
2022-02-10 19:54:14 +0530

    Create ArrayList_Example.java

---
+++ ArrayList_Example.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayList_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer>al=new ArrayList<>();
+		al.add(12);
+		al.add(30);
+		al.add(20);
+		al.add(38);
+		al.add(12);
+		System.out.println(al);
+		al.remove(2);
+		System.out.println(al);
+		
+	}
+
+}
