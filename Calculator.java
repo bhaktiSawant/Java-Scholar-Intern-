fa160c0ea87eb0610946466ab5a213b1d1416714
Bhakti Sawant <71435451+bhaktiSawant@users.noreply.github.com>
2022-02-10 19:43:11 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+public class Calculator {
+	public static double calculate(double a,char op,double b) {
+		double res=0;
+		switch(op) {
+		case '+':
+			res=a+b;
+			break;
+		case '-':
+			res=a-b;
+			break;
+		case '*':
+			res=a*b;
+			break;
+		case '/':
+			res=a/b;
+			break;
+		}
+		return res;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		double a=sc.nextInt();
+		char op=sc.next().charAt(0);
+		double b=sc.nextInt();
+		sc.close();
+		System.out.printf("%.2f",calculate(a,op,b));
+	}
+
+}
 
